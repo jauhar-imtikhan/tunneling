2807771eca0aeef2c51fe6fcc03d678dd8265184
jauhar-imtikhan <bangjoe.muns@gmail.com>
2023-10-15 14:20:21 +0700

    push kedua

--- update/menu.sh
+++ update/menu.sh
@@ -71,7 +71,7 @@ echo -e "$y-------------------------------------------------$wh"
 echo -e " \e[1;34mClient Name \e[0m: $Name"
 echo -e " \e[1;34mExpired     \e[0m: $Exp2"
 echo -e "$y-------------------------------------------------$wh"
-echo -e "$yl            Telegram : t.me/givpn$wh"
+echo -e "$yl            Telegram : t.me/jauhar_269$wh"
 echo -e "$y-------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 20 ] : " menu
 case $menu in
